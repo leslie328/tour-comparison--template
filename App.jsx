cc8baa4f6b73e3fcb6023736c4b28295bb3f6587
Leslie Rodriguez <leslie286@usf.edu>
2024-11-24 20:57:08 -0600

    commit

--- App.jsx
+++ App.jsx
@@ -7,3 +7,9 @@ const app =() => {
       <Router> {/* Wrap the app with Router */}
      <Routes> {/* Define routes  */}
 <Route path="/" element={<h1>Welcome to the Tour Comparison App!</h1>} />
+<Route path="/gallery" element={<Gallery />} />
+
+</Routes>
+</Router>
+    )};
+    export app;
