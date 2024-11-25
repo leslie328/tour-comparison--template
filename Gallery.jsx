a9b709d42ad99b3ca134a6e7bfda820c5f80b8b4
Leslie Rodriguez <leslie286@usf.edu>
2024-11-24 22:23:00 -0600

    Implement tour fetching and rendering with state management

--- Gallery.jsx
+++ Gallery.jsx
@@ -24,6 +24,14 @@ return (
               <p className="tour-info">
                   {tour.showDescription}
                    {/* "Not Interested" button */}
+                   <div key={tour.id} className="tour-2">
+              <img src={tour.image} alt={tour.name} className="tour-image" />
+              <div className="tour-details"></div>
+              <h2>{tour.name}</h2>
+              <p className="tour-price">Price: ${tour.price}</p>
+              <p className="tour-info">
+                  {tour.showDescription}
+                   {/* "Not Interested" button */}
                 <button onClick={() => handleRemoveTour(tour.1)}>Not Interested</button>
                 <button onClick={() =>  {tour.showDescription  'Show Less' : 'Read More'}
                 </button>
