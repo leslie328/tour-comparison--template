3d5c1bd04fc4ba54394a795ededdeab72bf1ba9a
Leslie Rodriguez <leslie286@usf.edu>
2024-11-24 22:08:09 -0600

    commit

--- Gallery.jsx
+++ Gallery.jsx
@@ -3,5 +3,26 @@ button from "./Button"
 Gallery (){
     [gallery, setGallery] =usestate ([])
 }
+   fetch url ='https://course-api.com/react-tours-project';
+fetch(url)
+then(response => response.json())
+
+return (
+    <div>
+      <h1> Tours </h1>
+      <div className="Tours">
+        {tours.length === 0 ? (
+          <p>No tours available at the moment.</p>  // If no tours are fetched
+        ) : (
+          tours.map((tour) => (
+            <div key={tour.id} className="tour-1">
+              <img src={tour.image} alt={tour.name} className="tour-image" />
+              <div className="tour-details"></div>
+              <h2>{tour.name}</h2>
+              <p className="tour-price">Price: ${tour.price}</p>
+              <p className="tour-info">
+                  {tour.showDescription}
+                   {/* "Not Interested" button */}
+                <button onClick={() => handleRemoveTour(tour.1)}>Not Interested</button>
 
 
